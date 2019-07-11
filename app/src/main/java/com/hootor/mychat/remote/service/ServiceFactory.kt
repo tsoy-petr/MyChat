074494a3cf1a3a1b467d372ee38f47bd62a212ee
bildovich <bildovich@yandex.ru>
2019-07-11 22:37:18 +1000

    3

--- app/src/main/java/com/hootor/mychat/remote/service/ServiceFactory.kt
+++ app/src/main/java/com/hootor/mychat/remote/service/ServiceFactory.kt
@@ -8,7 +8,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 object ServiceFactory {
-    const val BASE_URL = "https://chat.fandroid.info/rest_api/"
+    const val BASE_URL = "https://localhost/rest_api/"
 
     fun makeService(isDebug: Boolean): ApiService {
         val okHttpClient = makeOkHttpClient(
